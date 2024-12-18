9553844f5174007db7ad9fb9509f37a69e2709c2
Davide Gonnelli <davide.gonnelli1@gmail.com>
2024-12-18 22:49:28 +0100

    array

---
+++ snack-1/script.js
@@ -0,0 +1,23 @@
+// SNACK 1
+/* Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare a schermo la bici con peso minore. */
+
+// Array bici
+const bikes = [
+    {
+        brandName: "Grazziella",
+        weight: 1000
+    },
+
+    {
+      brandName: "Pinarello",
+      weight: 3000
+    },
+
+    {
+        brandName: "Atala",
+        weight: 600
+    }
+
+];
